be1992be9f7f56238a0c9e2600b0a20ec47a8e0f
Mike Oram <Mikeoram01@gmail.com>
2022-10-31 13:40:14 +0000

    added stylesheet

--- templates/homepage.php
+++ templates/homepage.php
@@ -11,7 +11,7 @@ $displayCards = new DisplayCards;
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="Mike Orams PHP Collectors App - Rewritten using Slim framework and MVC" />
     <title>MTG Card Collector App</title>
-    <link href='//fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+    <link href='../public/index.css'>
 </head>
 <body>
     <header class="headerContainer">
